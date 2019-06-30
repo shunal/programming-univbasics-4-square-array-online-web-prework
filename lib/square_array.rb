41500420d8a5e8f44bfdb8a3986141176657957d
Steven Hunal <stevehunal@gmail.com>
2019-06-30 19:27:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   counter = 0 
   
   while array[counter] do
-    puts array[counter] ** 2
+    array[counter] ** 2 = new_numbers
+    puts new_numbers
     counter += 1
   end
 end
